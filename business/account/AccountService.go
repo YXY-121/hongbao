b76690f9486ea82e1fd765677c73fa55e6f2d137
yanxy03 <yanxy03@mingyuanyun.com>
2021-07-01 17:43:16 +0800

    初步架构

---
+++ business/account/AccountService.go
@@ -0,0 +1,21 @@
+package account
+//账号服务
+type AccountService interface {
+	//根据用户id获取其对应的账号
+	GetAccountById(id int)
+
+	//查询用户Id对应账户里的余额
+	GetAccountBalanceById(id int) float64
+
+	//判断余额是否小于当前操作金e
+	IsLitterThenOperatingMone(id int,float642 float64) bool
+
+	//转账
+	Transfer(nowId int,targetId int,money float64)
+
+	//账户加钱
+	AddAccountBalance(id int,money float64)
+
+	//账户扣款
+	DecreaseAccountBalance(id int,money float64)
+}
--- business/account/repository/mysql/account.go
+++
@@ -1,5 +0,0 @@
-package mysql
-//type Account struct {
-//
-//}//这里写映射
-//respository里写服务
